03389bf79ea02c760dada049586af0651818ab6f
BuildTools <unconfigured@null.spigotmc.org>
2023-06-08 22:10:06 -0400

    Initial commit

---
+++ src/main/java/com/drewsullens/enhancedintro/EnhancedIntro.java
@@ -0,0 +1,19 @@
+package com.drewsullens.enhancedintro;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class EnhancedIntro implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/drewsullens/enhancedintro/client/EnhancedIntroClient.java
@@ -0,0 +1,13 @@
+package com.drewsullens.enhancedintro.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedIntroClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
